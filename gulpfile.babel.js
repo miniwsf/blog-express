b268342849232f86202ab5c0c7550e51674b8cee
wsf <714225540@qq.com>
2017-09-27 21:46:11 +0800

    修改样式

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import gulpSass from 'gulp-sass';
 
 gulp.task('default', ['sass'],() => {
   return gulp.src(['**/*.js','!app.js','!dist/**/*.js','!gulpfile.babel.js','!public/**/*.js','!node_modules/**/*.js'])
@@ -9,8 +8,3 @@ gulp.task('default', ['sass'],() => {
 });
 
 /*编译sass*/
-gulp.task('sass', () => {
-    return gulp.src(['**/*.scss'])
-        .pipe(gulpSass())
-        .pipe(gulp.dest('public/css'));
-});

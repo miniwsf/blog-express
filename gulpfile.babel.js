05cf27623be576a2905cfa3b94397f9d8ed37b31
wangsifan <wangsifan@xiaomi.com>
2017-09-27 18:54:56 +0800

    编译sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import gulpSass from 'gulp-sass';
 
-gulp.task('default', () => {
+gulp.task('default', ['sass'],() => {
   return gulp.src(['**/*.js','!app.js','!dist/**/*.js','!gulpfile.babel.js','!public/**/*.js','!node_modules/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('dist'));
+});
+
+/*编译sass*/
+gulp.task('sass', () => {
+    return gulp.src(['**/*.scss'])
+        .pipe(gulpSass())
+        .pipe(gulp.dest('public/css'));
 });

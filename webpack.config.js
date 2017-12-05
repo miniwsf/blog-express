e569d8d8cf9a2d9c92e5db331bafdebadd23cdc9
wangsifan <wangsifan@xiaomi.com>
2017-12-05 14:15:35 +0800

    webpack配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,79 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
 
-/*webpack配置文件*/
-module.exports = {
-    entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
-    output: {
-        path: __dirname + "/build",
-        filename: "bundle-[hash].js"
+const ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+const WebpackChunkHash = require("webpack-chunk-hash");
+const UglifyJsPlugin=require("uglifyjs-webpack-plugin");
+
+/* Shared Dev & Production */
+const config = {
+    context: path.resolve(__dirname, "routes"),
+
+    entry: {
+        index: "./index.js"
     },
-    devtool: 'none',
-    devServer: {
-        contentBase: "./public", //本地服务器所加载的页面所在的目录
-        historyApiFallback: true, //不跳转
-        inline: true,
-        hot: true
-    },
+
     module: {
-
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|svg|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: { name: "[name].[hash].[ext]" },
+                },
+            },
+        ],
     },
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        publicPath: "/",
+    },
+
+    resolve: {
+        extensions: [".js"],
+        modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+
     plugins: [
-        new webpack.ProvidePlugin({
-            $: 'public/js/jquery.min.js',
+        // Use "vendor" bundle as global commons chunk
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
         }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                pure_funcs: ["console.log"]
+            },
+            sourceMap: false
+        })
     ],
+
+    devServer: {
+        historyApiFallback: true,
+    },
 };
+
+if (process.env.NODE_ENV === "production") {
+    config.output.filename = "[name].[chunkhash].js";
+    config.plugins = [
+        ...config.plugins,
+        new webpack.HashedModuleIdsPlugin(),
+        new WebpackChunkHash(),
+        new ChunkManifestPlugin({
+            filename: "chunk-manifest.json",
+            manifestVariable: "webpackManifest",
+            inlineManifest: true,
+        }),
+    ];
+}
+
+module.exports = config;

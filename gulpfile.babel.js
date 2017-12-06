81fa378eb5225bfcc4b086a0b372d1474953567a
wangsifan <wangsifan@xiaomi.com>
2017-12-06 10:40:37 +0800

    delete the code of useless in gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,31 +6,3 @@ gulp.task("default", () =>{
     .pipe(babel())
     .pipe(gulp.dest("dist"));
 });
-
-/*babel*/
-/*gulp.task("babel",() => {
-    return gulp.src(["**!/!*.js","!app.js","!dist/!**!/!*.js","!gulpfile.babel.js","!public/!**!/!*.min.js","!node_modules/!**!/!*.js"])
-        .pipe(babel())
-        .pipe(gulp.dest("dist"));
-});
-
-/!*压缩*!/
-gulp.task("uglify",() => {
-    return gulp.src(["dist/!**!/!*.js"])
-        .pipe(babel())
-        .pipe(gulp.dest("dist"));
-});
-
-/!*测试*!/
-gulp.task("dev",["babel"],() => {
-    return gulp.src(["**!/!*.js","!app.js","!dist/!**!/!*.js","!gulpfile.babel.js","!public/!**!/!*.js","!node_modules/!**!/!*.js"])
-        .pipe(babel())
-        .pipe(gulp.dest("dist"));
-});
-
-/!*上线*!/
-gulp.task("dist",() => {
-    return gulp.src(["**!/!*.js","!app.js","!dist/!**!/!*.js","!gulpfile.babel.js","!public/!**!/!*.js","!node_modules/!**!/!*.js"])
-        .pipe(babel())
-        .pipe(gulp.dest("dist"));
-});*/

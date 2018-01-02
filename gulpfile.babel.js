b6538300470c81423d651b0fda65c374dfd0bda6
wangsifan <wangsifan@xiaomi.com>
2018-01-02 15:45:17 +0800

    recode

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,35 @@
 import gulp from "gulp";
 import babel from "gulp-babel";
+import cleanCSS from "gulp-clean-css";
+import jsmin from "gulp-jsmin";
 
-gulp.task("default", () =>{
-    return gulp.src(["**/*.js","!app.js","!dist/**/*.js","!gulpfile.babel.js","!public/**/*.js","!node_modules/**/*.js"])
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+gulp.task("default", ["babel","public","sass","js","image"]);
+
+gulp.task("babel", () =>{
+    return gulp.src(["**/*.js","!app.js","!dist/**/*.js","!gulpfile.babel.js","!public/**/*.js","!node_modules/**/*.js","!src/**/*.js"])
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
 });
+
+gulp.task("sass",()=>{
+    return gulp.src(["src/css/*.css"])
+        .pipe(cleanCSS({compatibility: "ie8"}))
+        .pipe(gulp.dest("dist/public/css"));
+});
+
+gulp.task("js",()=>{
+    return gulp.src(["src/js/**/*.js"])
+        .pipe(jsmin())
+        .pipe(gulp.dest("dist/public/js"));
+});
+
+gulp.task("image",()=>{
+    return gulp.src(["src/images/**"])
+        .pipe(gulp.dest("dist/public/images"));
+});
+
+gulp.task("public",()=>{
+    return gulp.src(["public/**"])
+        .pipe(gulp.dest("dist"));
+});
+
